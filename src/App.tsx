ba09059f784b245bb2454a91f4ec48089a2afa80
Stafford Williams <stafford.williams@gmail.com>
2020-08-22 19:16:27 +1000

    speed dial

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,56 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Game } from './Game';
-import './App.css';
-import { Colors } from '@staff0rd/typescript'
+import { Colors } from '@staff0rd/typescript';
 import * as PIXI from 'pixi.js';
 import { Config } from './Config';
+import { Toolbar } from './components/Toolbar';
+import { makeStyles } from '@material-ui/core/styles';
 
 const getConfig: (() => Config) = () => ({
   tileSize: 50,
 });
 
-const App = () => {
-  const pixiUpdate = (element: HTMLDivElement) => {
+const useStyles = makeStyles(() => ({
+  root: {
+    width: '100%',
+    height: '100%',
+  },
+  pixi: {
+    width: '100%',
+    height: '100%',
+  },
+}));
 
+const app = new PIXI.Application({
+  width: window.innerWidth,
+  height: window.innerHeight,
+  backgroundColor: Colors.BlueGrey.C900
+});
+
+const App = () => {
+  const classes = useStyles();
+  useEffect(() => {
+    window.onresize = () => {
+      app.view.width = window.innerWidth;
+      app.view.height = window.innerHeight;
+    };
+  }, []);
+
+  const pixiUpdate = (element: HTMLDivElement) => {
     if (element && element.children.length <= 0) {
-        const app = new PIXI.Application({width: window.innerWidth, height: window.innerHeight, backgroundColor: Colors.BlueGrey.C900 });
+        
         element.appendChild(app.view);
         const game = new Game(getConfig(), app);
         game.init();
     }
-}
+  }
 
-  return (<div>
-      <div ref={pixiUpdate} />
+  return (
+    <div className={classes.root}>
+      <div className={classes.pixi} ref={pixiUpdate} />
+      <Toolbar />
     </div>
-    );
+  );
 }
 
 export default App;
---
+++ src/components/Toolbar.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import SpeedDial, { SpeedDialProps } from '@material-ui/lab/SpeedDial';
+import SpeedDialIcon from '@material-ui/lab/SpeedDialIcon';
+import SpeedDialAction from '@material-ui/lab/SpeedDialAction';
+import SellIcon from '@material-ui/icons/AttachMoney';
+import BuyIcon from '@material-ui/icons/ShoppingCart';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      transform: 'translateZ(0px)',
+      flexGrow: 1,
+    },
+    exampleWrapper: {
+      position: 'relative',
+      marginTop: theme.spacing(3),
+      height: 380,
+    },
+    radioGroup: {
+      margin: theme.spacing(1, 0),
+    },
+    speedDial: {
+      position: 'absolute',
+      '&.MuiSpeedDial-directionUp, &.MuiSpeedDial-directionLeft': {
+        bottom: theme.spacing(2),
+        right: theme.spacing(2),
+      },
+      '&.MuiSpeedDial-directionDown, &.MuiSpeedDial-directionRight': {
+        top: theme.spacing(2),
+        left: theme.spacing(2),
+      },
+    },
+  }),
+);
+
+const actions = [
+  { icon: <BuyIcon />, name: 'Buy' },
+  { icon: <SellIcon />, name: 'Sell' },
+];
+
+export const Toolbar = () => {
+  const classes = useStyles();
+  const [direction] = React.useState<SpeedDialProps['direction']>('up');
+  const [open, setOpen] = React.useState(false);
+  const [hidden] = React.useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  return (
+    <SpeedDial
+      ariaLabel="SpeedDial example"
+      className={classes.speedDial}
+      hidden={hidden}
+      icon={<SpeedDialIcon />}
+      onClose={handleClose}
+      onOpen={handleOpen}
+      open={open}
+      direction={direction}
+    >
+      {actions.map((action) => (
+        <SpeedDialAction
+          key={action.name}
+          icon={action.icon}
+          tooltipTitle={action.name}
+          onClick={handleClose}
+        />
+      ))}
+    </SpeedDial>
+  );
+}
