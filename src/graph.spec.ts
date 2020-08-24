c293110fb961596eb4fd2685d044211209c12685
Stafford Williams <stafford.williams@gmail.com>
2020-08-24 17:55:51 +1000

    return links & nodes

--- src/graph.spec.ts
+++ src/graph.spec.ts
@@ -3,17 +3,17 @@ import { getNodeByPoint, generateGrid } from "./graph";
 describe('generateGrid', () => {
     describe('generate', () => {
         it ('should set x in id', () => {
-            const graph = generateGrid(0, 0, 2, 1, () => true);
+            const { graph } = generateGrid(0, 0, 2, 1, () => true);
             const node = getNodeByPoint(graph, 1, 0);
             expect(node).not.toBeUndefined();
         });
         it ('should set y in id', () => {
-            const graph = generateGrid(0, 0, 1, 2, () => true);
+            const { graph } = generateGrid(0, 0, 1, 2, () => true);
             const node = getNodeByPoint(graph, 0, 1);
             expect(node).not.toBeUndefined();
         });
         it ('should set data', () => {
-            const graph = generateGrid(0, 0, 2, 2, () => ({ something: true}));
+            const { graph } = generateGrid(0, 0, 2, 2, () => ({ something: true}));
             graph.forEachNode(n => {
                 expect(n).toHaveProperty("data");
                 expect(n.data).toHaveProperty('something');
--- src/graph.ts
+++ src/graph.ts
@@ -1,4 +1,4 @@
-import createGraph, { Graph, Node } from 'ngraph.graph';
+import createGraph, { Graph, Node, Link } from 'ngraph.graph';
 
 export function nodeId(x: number, y: number) {
     return `${x}-${y}`;
@@ -6,36 +6,57 @@ export function nodeId(x: number, y: number) {
 
 export function getNodeByPoint<NodeData, LinkData>(graph: Graph<NodeData, LinkData>, x: number, y: number) {
     const id = nodeId(x, y);
-    const nodes: Node<NodeData>[] = [];
-    graph.forEachNode(n => { nodes.push(n); });
+    const nodes: Node<NodeData>[] = getNodes<NodeData, LinkData>(graph);
     return nodes.filter(n => n.id === id)[0];
 }
 
-export function generateGrid<NodeData>(
+export function getNodes<NodeData, LinkData>(graph: Graph<NodeData, LinkData>) {
+    const nodes: Node<NodeData>[] = [];
+    graph.forEachNode(n => { nodes.push(n); });
+    return nodes;
+}
+
+export function getLinks<NodeData, LinkData>(graph: Graph<NodeData, LinkData>) {
+    const links: Link<LinkData>[] = [];
+    graph.forEachLink(l => { links.push(l); });
+    return links;
+}
+
+export function generateGrid<NodeData, LinkData>(
     startX: number,
     startY: number,
     width: number,
     height: number, 
-    data: (x: number, y: number) => NodeData) {
+    data: (x: number, y: number) => NodeData,
+    graph: Graph<NodeData, LinkData> = createGraph<NodeData, LinkData>()) {
+
+    const links: Link<LinkData>[] = [];
+    const nodes: Node<NodeData>[] = [];
     
     if (height < 1 || width < 1) {
         throw new Error("Invalid number of nodes in grid graph");
     }
-    
-    var g = createGraph();
 
     if (height === 1 && width === 1) {
-        g.addNode(nodeId(startX, startY), data(startX, startY));
-        return g;
+        const node = graph.addNode(nodeId(startX, startY), data(startX, startY));
+        nodes.push(node);
+        return { graph, nodes, links };
     }
 
     for (let i = 0; i < height; ++i) {
-    for (let j = 0; j < width; ++j) {
-        g.addNode(nodeId(j, i), data(j, i))
-        if (i > 0) { g.addLink(nodeId(j, i), nodeId(j, i -1)); }
-        if (j > 0) { g.addLink(nodeId(j, i), nodeId(j-1, i)); }
-    }
+        for (let j = 0; j < width; ++j) {
+            const node = graph.addNode(nodeId(j, i), data(j, i))
+            nodes.push(node);
+            if (i > 0) { 
+                const link = graph.addLink(nodeId(j, i), nodeId(j, i -1));
+                links.push(link);
+            }
+            if (j > 0) {
+                const link = graph.addLink(nodeId(j, i), nodeId(j-1, i));
+                links.push(link);
+            }
+        }
     }
 
-    return g;
+    return { graph, nodes, links };
 }
